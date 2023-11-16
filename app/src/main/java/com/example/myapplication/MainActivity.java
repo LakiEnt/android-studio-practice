e5c65b84e45a229861b400d1c5664bb7b9f53cac
lakient <modenoff.nik@gmail.com>
2023-11-16 13:15:05 +0500

    lab-zad-6

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,88 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.Manifest;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-import com.journeyapps.barcodescanner.ScanContract;
-import com.journeyapps.barcodescanner.ScanOptions;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import android.content.Intent;
-import android.graphics.Bitmap;
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView image;
-    private Button button;
+    private ListView listView;
+    private DBHelper helper;
+    private SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = findViewById(R.id.imageView);
-        button = findViewById(R.id.button);
+        helper=new DBHelper(getApplicationContext());
+        try {
+            database=helper.getWritableDatabase();
+        } catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+        Button button = findViewById(R.id.button);
+        listView = findViewById(R.id.listview);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                            Manifest.permission.CAMERA
-                    },100);
-                }
-                else {
-                    ScanOptions options = new ScanOptions();
-                    options.setPrompt("Скан кода");
-                    options.setBeepEnabled(true);
-                    options.setOrientationLocked(true);
-                    options.setCaptureActivity(CaptureAct.class);
+            public void onClick(View v) {
+                ArrayList<HashMap<String,String>> list=new ArrayList<>();
+                HashMap <String, String> list_item;
 
-                    barLauncher.launch(options);
+                Cursor cursor = database.rawQuery("SELECT id,name,quantity FROM clothes",null);
+                cursor.moveToFirst();
 
+                while (!cursor.isAfterLast()){
+                    list_item=new HashMap<>();
+                    list_item.put("name", cursor.getString(1));
+                    list_item.put("quantity", cursor.getString(2));
+
+                    list.add(list_item);
+                    cursor.moveToNext();
                 }
-//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                startActivityForResult(intent, 100);
+                cursor.close();
+
+                SimpleAdapter adapter = new SimpleAdapter(
+                        getApplicationContext(),
+                        list, android.R.layout.simple_list_item_2,
+                        new String[]{"name", "time"},
+                        new int[] {android.R.id.text1, android.R.id.text2}
+                );
+                listView.setAdapter(adapter);
             }
         });
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100) {
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            image.setImageBitmap(bitmap);
-        }
-    }
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result ->{
-        if (result.getContents()!=null){
-              String qrText = result.getContents().toString();
-//              String qrText = "123";
-
-
-            if(qrText == "улица") {
-                Intent intent = new Intent(MainActivity.this, StreetActivity.class);
-                startActivity(intent);
-            } else if (qrText == "аптека" ) {
-                Intent intent = new Intent(MainActivity.this, HealActivity.class);
-                startActivity(intent);
-            } else if (qrText == "фонарь") {
-                Intent intent = new Intent(MainActivity.this, LightActivity.class);
-                startActivity(intent);
-            }
-            else {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Нет такой страницы");
-                builder.setMessage(qrText);
-                builder.show();
-            }
-        }
-    });
 }
