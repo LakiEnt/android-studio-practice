111632191eb75a8fc89071674e67f9948ee8ceb1
lakient <modenoff.nik@gmail.com>
2023-11-20 10:16:52 +0500

    lab-zad-6

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -9,59 +9,54 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class DBHelper extends SQLiteOpenHelper {
-    private static String DB_NAME = "animal.db";
+public class DBHelper extends SQLiteOpenHelper{
+    private static String DB_NAME="animal.db";
     private static String DB_LOCATION;
-    private static final int DB_VERSION = 1;
-
+    private static final int DB_VERSION=10;
     private final Context myContext;
 
-    public DBHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
-        this.myContext = context;
-        DB_LOCATION=context.getApplicationInfo().dataDir+"/database/";
+    public  DBHelper(Context context){
+        super(context,DB_NAME,null,DB_VERSION);
+        this.myContext=context;
+        DB_LOCATION=context.getApplicationInfo().dataDir+"/databases/";
 
         copyDB();
     }
-    private boolean checkDB() {
+    private boolean checkDB(){
         File fileDB = new File(DB_LOCATION+DB_NAME);
-        System.out.println(DB_LOCATION + DB_NAME);
-        System.out.println(fileDB.exists());
         return fileDB.exists();
     }
+
     private void copyDB(){
-        if (!checkDB()){
+        if(checkDB()){
             this.getReadableDatabase();
             try {
                 copyDBFile();
+            } catch (IOException e){
+
             }
-            catch (IOException e){}
         }
     }
-    private void copyDBFile() throws IOException{
+    private void copyDBFile() throws IOException {
         InputStream inputStream = myContext.getAssets().open(DB_NAME);
         OutputStream outputStream = new FileOutputStream(DB_LOCATION + DB_NAME);
-
-        System.out.println(inputStream);
-        System.out.println(outputStream);
-
-        byte[] buffer = new byte[1024];
+        byte[] buffer=new byte[1024];
         int length;
-        while ((length = inputStream.read(buffer)) > 0){
-            outputStream.write(buffer, 0, length);
+        while ((length=inputStream.read(buffer))>0){
+            outputStream.write(buffer,0,length);
         }
         outputStream.flush();
         outputStream.close();
         inputStream.close();
     }
 
-
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
     }
+
     @Override
-    public void  onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-
-import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,47 +16,93 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private DBHelper helper;
     private SQLiteDatabase database;
+    private Button food_btn,clothes_btn,game_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        listView=findViewById(R.id.listView1);
+        food_btn=findViewById(R.id.button2);
+        clothes_btn=findViewById(R.id.button);
+        game_btn=findViewById(R.id.button3);
         helper=new DBHelper(getApplicationContext());
-        try {
+        try{
             database=helper.getWritableDatabase();
-        } catch (Exception e){
+        }catch (Exception e){
             e.printStackTrace();
-
         }
 
-        Button button = findViewById(R.id.button);
-        listView = findViewById(R.id.listview);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        clothes_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                ArrayList<HashMap<String,String>> list=new ArrayList<>();
-                HashMap <String, String> list_item;
-
+            public void onClick(View view) {
+                ArrayList<HashMap<String,String>> clothes = new ArrayList<>();
+                HashMap<String,String> animal;
                 Cursor cursor = database.rawQuery("SELECT id,name,quantity FROM clothes",null);
                 cursor.moveToFirst();
-
-                while (!cursor.isAfterLast()){
-                    list_item=new HashMap<>();
-                    list_item.put("name", cursor.getString(1));
-                    list_item.put("quantity", cursor.getString(2));
-
-                    list.add(list_item);
+                while(!cursor.isAfterLast()){
+                    animal=new HashMap<>();
+                    animal.put("name", cursor.getString(1));
+                    animal.put("quantity", cursor.getString(2));
+                    clothes.add(animal);
                     cursor.moveToNext();
                 }
                 cursor.close();
 
                 SimpleAdapter adapter = new SimpleAdapter(
-                        getApplicationContext(),
-                        list, android.R.layout.simple_list_item_2,
-                        new String[]{"name", "time"},
-                        new int[] {android.R.id.text1, android.R.id.text2}
+                        getApplicationContext(),clothes, android.R.layout.simple_list_item_2,
+                        new String[]{"name","quantity"}, new int[]{android.R.id.text1,android.R.id.text2}
+
+                );
+                listView.setAdapter(adapter);
+            }
+        });
+
+        game_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<HashMap<String,String>> games = new ArrayList<>();
+                HashMap<String,String> game;
+                Cursor cursor = database.rawQuery("SELECT id,name,quantity FROM games",null);
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()){
+                    game=new HashMap<>();
+                    game.put("name", cursor.getString(1));
+                    game.put("quantity", cursor.getString(2));
+                    games.add(game);
+                    cursor.moveToNext();
+                }
+                cursor.close();
+
+                SimpleAdapter adapter = new SimpleAdapter(
+                        getApplicationContext(),games, android.R.layout.simple_list_item_2,
+                        new String[]{"name","quantity"}, new int[]{android.R.id.text1,android.R.id.text2}
+
+                );
+                listView.setAdapter(adapter);
+            }
+        });
+
+        food_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<HashMap<String,String>> foods = new ArrayList<>();
+                HashMap<String,String> food;
+                Cursor cursor = database.rawQuery("SELECT id,name,quantity FROM foods",null);
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()){
+                    food=new HashMap<>();
+                    food.put("name", cursor.getString(1));
+                    food.put("quantity", cursor.getString(2));
+                    foods.add(food);
+                    cursor.moveToNext();
+                }
+                cursor.close();
+
+                SimpleAdapter adapter = new SimpleAdapter(
+                        getApplicationContext(),foods, android.R.layout.simple_list_item_2,
+                        new String[]{"name","quantity"}, new int[]{android.R.id.text1,android.R.id.text2}
+
                 );
                 listView.setAdapter(adapter);
             }
